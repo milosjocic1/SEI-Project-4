216887d101bceac4b1fef493c33b8089a2ea36de
ellieeadington <63414517+ellieeadington@users.noreply.github.com>
2022-10-21 16:10:42 +0100

    created the User, Transaction & Seller models

---
+++ models/Seller.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const sellerSchema = mongoose.Schema({
+
+    sellerName: {
+        type: String,
+        minlength: [3],
+        maxlength: [99],
+        required: true
+    },
+    bio: {
+        type: String,
+        minlength: [3],
+        maxlength: [500], 
+        required: true       
+    },
+    profilePhoto: {
+        type: String,
+        required: true
+    },
+    user: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],   
+    product: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    }],   
+    review: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Review'
+    }],   
+},
+{
+    timestamps: true   
+});
+
+const Seller = mongoose.model('Seller', sellerSchema);
+
+module.exports = {Seller};
