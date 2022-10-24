64f21a920f308426fa81da98e30368d84c773609
ellieeadington <63414517+ellieeadington@users.noreply.github.com>
2022-10-24 19:49:06 +0100

    committing changes for merge

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,13 @@ const {User} = require("../models/User")
 const {Favourite} = require("../models/Favourite")
 const {Review} = require("../models/Review")
 
+
 // __________________________________ USER DASHBOARD GET  __________________________________ //
 
 exports.user_dashboard_get = async (req, res) => {
-    let user = await User.findById("63541db8b75e63463d5178b2")
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    user = decodedToken.user;
+    let user = await User.findById({user._u})}
     .populate("favourite review")
     try {
         res.status(200).json({user})
