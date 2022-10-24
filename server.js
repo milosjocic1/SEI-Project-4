24f6697af02fff70fae8cc87f489d9fd1307b8d0
alex-sasha-ward <amwtranslations@gmail.com>
2022-10-24 19:47:14 +0100

    Updates route imports

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ const usersRouter = require("./routes/users");
 const productRouter = require("./routes/products");
 const cartRouter = require("./routes/cart");
 const reviewRouter = require("./routes/reviews");
-const sellersRouter = require("./routes/reviews");
-const transactionRouter = require("./routes/reviews");
+const sellersRouter = require("./routes/sellers");
+const transactionRouter = require("./routes/transactions");
 
 
 
@@ -69,8 +69,6 @@ app.use("/", cartRouter);
 app.use("/", reviewRouter);
 app.use("/", transactionRouter);
 app.use("/", sellersRouter);
-// app.use("/", cartRouter);
-// app.use("/", cartRouter);
 
 app.set("view engine", "ejs");
 
