23a7e4c98a08228f8285abf31a7fc62d3e548371
Claudia Boden <clauboden@hotmail.com>
2022-10-26 00:57:06 +0100

    pull

--- controllers/products.js
+++ controllers/products.js
@@ -22,7 +22,7 @@ exports.product_create_post = (req, res) => {
   // res.send("POST WORKS")
   // Saving the data into the database
   let product = new Product(req.body);
-  // product.seller.push(req.query.id)
+  product.seller.push(req.query.id);
   product
     .save()
     .then(() => {
--- models/Product.js
+++ models/Product.js
@@ -29,7 +29,7 @@ const productSchema = mongoose.Schema({
         type: String,
         required: true
     },
-    // images: [],
+    images: [],
     shippingRate: String,
     returnsPolicy: {
         type: String,
