036844990ef87d1b1042a3c7db8840aefc2a1651
Milos Jocic <milosjocicpt@gmail.com>
2022-10-27 17:04:58 +0100

    tryna upload img to product

--- models/Product.js
+++ models/Product.js
@@ -29,7 +29,7 @@ const productSchema = mongoose.Schema({
         type: String,
         required: true
     },
-    images: [],
+    cloudinary_url: String,
     shippingRate: String,
     returnsPolicy: {
         type: String,
