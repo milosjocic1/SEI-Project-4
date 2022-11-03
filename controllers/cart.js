28ed01d5e7a086ad3a176770303fa95e591537a1
ellieeadington <63414517+ellieeadington@users.noreply.github.com>
2022-11-03 17:36:31 +0000

    tidied up code in cart.js

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,12 +1,12 @@
-const {Cart} = require("../models/Cart");
-const {User} = require("../models/User");
-const {Product} = require("../models/Product");
+const { Cart } = require("../models/Cart");
+const { User } = require("../models/User");
+const { Product } = require("../models/Product");
 const { isValidObjectId } = require("mongoose");
-const moment = require('moment');
+const moment = require("moment");
 
 exports.addItemToCart = async (req, res) => {
   let userId = req.query.userId.trim();
-  console.log("user id is " + userId)
+  console.log("user id is " + userId);
   let user = await User.exists({ _id: userId });
   if (!userId || !isValidObjectId(userId) || !user)
     return res.status(400).send({ status: false, message: "Invalid user ID" });
@@ -15,41 +15,38 @@ exports.addItemToCart = async (req, res) => {
 
   if (!product)
     return res.status(400).send({ status: false, message: "Invalid product" });
-
   else {
     product = Product.findById(req.query.productId)
-    .then( async (product) => {
-      let cart = await Cart.findOne({userId: userId});
+      .then(async (product) => {
+        let cart = await Cart.findOne({ userId: userId });
 
         if (cart) {
-            let i = cart.products.findIndex((p) => p.productId == product);
-        
-            if (i > -1) {
-              let productItem = cart.products[i];
-              productItem.quantity += 1;
-              cart.products[i] = productItem;
-            } else {
-          cart.products.push({ productId: product, quantity: 1 });  
-          cart.save();   
-          return res.status(200).send({ status: true, updatedCart: cart });
-        }
-        } 
-        else {
+          let i = cart.products.findIndex((p) => p.productId == product);
+
+          if (i > -1) {
+            let productItem = cart.products[i];
+            productItem.quantity += 1;
+            cart.products[i] = productItem;
+          } else {
+            cart.products.push({ productId: product, quantity: 1 });
+            cart.save();
+            return res.status(200).send({ status: true, updatedCart: cart });
+          }
+        } else {
           const newCart = Cart.create({
             userId,
             products: [{ productId: product, quantity: 1 }],
           });
-      
+
           return res.status(201).send({ status: true, newCart: newCart });
         }
-    }) 
-    .catch((err) => {
-      console.log(err)
-    })
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
+};
 
-}
-      
 exports.getCart = async (req, res) => {
   let userId = req.query.userId;
   let user = await User.findById(userId);
@@ -60,30 +57,29 @@ exports.getCart = async (req, res) => {
   let cart = await Cart.findOne({ userId: userId })
     .populate("products.productId")
     .then((cart) => {
-      return cart
-  })
+      return cart;
+    });
 
   try {
-    if (!cart){
+    if (!cart) {
       return res
-      .status(404)
-      .send({ status: false, message: "Cart not found for this user" });
-    } 
-    else{
+        .status(404)
+        .send({ status: false, message: "Cart not found for this user" });
+    } else {
       let total = 0;
       cart.products.map((product) => {
-        total += ((product.quantity * product.productId.price) + product.productId.shippingRate)
-        return total
-      })
+        total +=
+          product.quantity * product.productId.price +
+          product.productId.shippingRate;
+        return total;
+      });
       cart.total = total;
-      res.status(200).json({cart});
+      res.status(200).json({ cart });
     }
-
   } catch (error) {
     console.log(error);
   }
 };
-
 
 // exports.decreaseQuantity = async (req, res) => {
 //   // use add product endpoint for increase quantity
@@ -146,43 +142,44 @@ exports.removeItem = async (req, res) => {
     return res.status(400).send({ status: false, message: "Invalid user ID" });
 
   let product = Product.findById(req.query.productId);
-  let productId = req.query.productId
+  let productId = req.query.productId;
 
   if (!product)
     return res.status(400).send({ status: false, message: "Invalid product" });
-
   else {
     product = Product.findById(req.query.productId)
-    .then( async (product) => {
-      let cart = await Cart.findOne({userId: userId})
-      
-        if (cart)
-        console.log(typeof productId)
-        
-            {
-              let i = cart.products.findIndex((p) => p.productId.toString() == productId);
-              console.log(typeof productId)
-              console.log(i)
-        
-          
-          cart.products.splice(i, 1);  
-          cart.save();   
-          console.log(cart)
-          return res.status(200).send({ status: true, updatedCart: cart });}
-    }) 
-    .catch((err) => {
-      console.log(err)
-    })
+      .then(async (product) => {
+        let cart = await Cart.findOne({ userId: userId });
+
+        if (cart) console.log(typeof productId);
+
+        {
+          let i = cart.products.findIndex(
+            (p) => p.productId.toString() == productId
+          );
+          console.log(typeof productId);
+          console.log(i);
+
+          cart.products.splice(i, 1);
+          cart.save();
+          console.log(cart);
+          return res.status(200).send({ status: true, updatedCart: cart });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
+};
 
 exports.shippingAndBilling = async (req, res) => {
-
-  console.log(req.body.addressLine1S)
-  console.log(req.body.addressLine1B)
+  console.log(req.body.addressLine1S);
+  console.log(req.body.addressLine1B);
   let userId = req.query.userId.trim();
-  let user = await User.findById(userId)
-  try{
-    await User.findByIdAndUpdate(user._id,
+  let user = await User.findById(userId);
+  try {
+    await User.findByIdAndUpdate(
+      user._id,
       {
         $set: {
           shippingAddress: {
@@ -190,29 +187,28 @@ exports.shippingAndBilling = async (req, res) => {
             addressLine2: req.body.addressLine2S,
             city: req.body.cityS,
             county: req.body.countyS,
-            postCode: req.body.postCodeS
-          }      
-        }},
+            postCode: req.body.postCodeS,
+          },
 
-        {new: true, runValidators: true,useFindAndModify: false}
-        )
-        await User.findByIdAndUpdate(user._id,
-          {
-            $set: {
-              billingAddress: {
-                addressLine1: req.body.addressLine1B,
-                addressLine2: req.body.addressLine2B,
-                city: req.body.cityB,
-                county: req.body.countyB,
-                postCode: req.body.postCodeB
-              }      
-            }},
-            {new: true, runValidators: true,useFindAndModify: false}
-            )
-
+          billingAddress: {
+            addressLine1: req.body.addressLine1B,
+            addressLine2: req.body.addressLine2B,
+            city: req.body.cityB,
+            county: req.body.countyB,
+            postCode: req.body.postCodeB,
+          },
+        },
+      },
+      { new: true, runValidators: true, useFindAndModify: false },
+      function (err, user) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.status(200).json({ user });
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error);
   }
-  catch(error){
-    console.log(error)
-  } 
-}  
-}
+};
--- routes/cart.js
+++ routes/cart.js
@@ -7,10 +7,12 @@ const router = express.Router();
 router.use(methodOverride('_method'));
 
 router.use(express.json());
+
 const cartCtrl = require("../controllers/cart");
 
 router.post('/cart', cartCtrl.addItemToCart);
 router.get('/cart', cartCtrl.getCart);
 router.delete('/cart', cartCtrl.removeItem);
+router.post('/shipping_billing/update', cartCtrl.shippingAndBilling)
 
 module.exports = router;
